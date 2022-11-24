b85e24c32faaaac89eaec5e3e86c62733661251e
Gustavo Pérez <yahoo_3257@hotmail.com>
2022-11-23 18:52:04 -0600

    Creando una pagina CRUD
    Web con CRUD usando JavaScript asíncrono

---
+++ MODULO 02; FRONT-END/_03_Sitios interactivos, integrando JavaScript al Navegador/MODULO 12/CRUD_JS_Async-clase05/service/client-service.js
@@ -0,0 +1,61 @@
+//const http = new XMLHttpRequest();
+//
+//-CRUD----Métodos HTTP
+//
+//Create    POST
+//Read      GET
+//Update    PU/PATH
+//Delete    DELETE
+//
+//http.open("GET","http://localhost:3000/perfil");recibe los datos alojados en el json y los abre
+//http.send();hace la comunicacion y envia la información 
+//console.log(http);
+//http.onload = () =>{//carga la informacion del json
+//  const data = http.response;
+//  console.log(data);
+//};
+
+const listaClientes = () =>
+  fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json());
+
+const crearCliente = (nombre, email) => {
+  return fetch("http://localhost:3000/perfil", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ nombre, email, id: uuid.v4() }),
+  });
+};
+
+const eliminarCliente = (id) => {
+  return fetch(`http://localhost:3000/perfil/${id}`, {
+    method: "DELETE",
+  });
+};
+
+const detalleCliente = (id) => {
+  return fetch(`http://localhost:3000/perfil/${id}`).then((respuesta) =>
+    respuesta.json()
+  );
+};
+
+const actualizarCliente = (nombre, email, id) => {
+  return fetch(`http://localhost:3000/perfil/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ nombre, email }),
+  })
+    .then((respuesta) => respuesta)
+    .catch((err) => console.log(err));
+};
+
+export const clientServices = {
+  listaClientes,
+  crearCliente,
+  eliminarCliente,
+  detalleCliente,
+  actualizarCliente,
+};
